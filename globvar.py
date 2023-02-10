cb5727c874457bf16b188501e67ec00d759fc771
Jan Lerking <jan.lerking@gmail.com>
2023-02-10 23:29:16 +0100

    Added global variables. /JL

---
+++ globvar.py
@@ -0,0 +1,5 @@
+import h2inc_AI as h2inc
+
+def init():
+    global ai
+    ai = h2inc.H2INC_AI("H2INC_AI")
--- h2inc_AI.py
+++ h2inc_AI.py
@@ -2,7 +2,7 @@
 class H2INC_AI:
     def __init__(self, name):
         self.name = name
-        self.Version = "0.0.1"
+        self.Version = "0.0.2"
         self.Copyright = "Copyright (c) 2023"
         self.Author = "Jan Lerking"
         self.Contact = "github.com/Lerking"
